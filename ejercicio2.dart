a3ad42b95e36c3728968e109a23e98c21221ec4c
OmarFDiaz <156007231+OmarFDiaz@users.noreply.github.com>
2024-06-08 22:58:35 +0000

    fix ejercicio 2

--- ejercicio2.dart
+++ ejercicio2.dart
@@ -1,6 +1,6 @@
-bool repetido(dynamic list) {
+bool repetido(List<int> list) {
   int cont = 0;
-  for (int i = 1; i < list.lenght; i++) {
+  for (int i = 1; i < list.length; i++) {
     if (list[i] == list[(i - 1)]) {
       cont += 1;
     }
